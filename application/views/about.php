480aaefc95a46581193180af762db78d6b756508
Nidhi <nidhikatkani3@gmail.com>
2024-09-30 17:54:35 +0530

    about page

--- application/views/about.php
+++ application/views/about.php
@@ -1,5 +1,110 @@
-<section   style="background-image: url('assets/img/bg.png'); max-width: 100%; background-repeat: no-repeat;height:90vh; ">
+<!-- <section   style="background-image: url('assets/img/bg.png'); max-width: 100%; background-repeat: no-repeat;height:90vh; ">
 <div class=" d-flex justify-content-center align-items-center container space" style="padding-top: 40vh;">
     <h1>COMING SOON</h1>
 </div>
+</section> -->
+<style>
+    .line {
+        font-size: 13px;
+    }
+
+    .phone1 {
+        z-index: 2;
+        margin-right: -30px;
+        /* Adjust this value to control overlap */
+
+    }
+
+    .phone2 {
+        z-index: 1;
+        margin-left: -20px;
+        /* Adjust this value to bring phone2 over phone1 */
+    }
+    .eicon1{
+        position:absolute;
+        top: 50%;
+        left: 10%;
+    } 
+    .eicon2{
+        position:absolute;
+        top: 50%;
+        right: 10%;
+    }
+</style>
+
+<!-- banner -->
+<section>
+    <div class="container py-5">
+        <div class="row d-flex justify-content-center pt-3">
+            <div class="col-md-3">
+                <img src="assets/img/about-logo.png" alt="">
+            </div>
+
+        </div>
+    </div>
+    <div class="container">
+        <div class="row  d-flex justify-content-center">
+            <div class="col-md-10 text-center">
+                <p>Aesthetics AI is India’s first Ai agent driven clinic management suite. Specifically designed for Dentists, Dermatologists and cosmetologists. We are fully committed to betterment of patients and doctors. We are the only one industry to ensure data privacy of each patient by white labelling our solution. All medical records of patients are stored, encrypted and never access by us. It remains with the consulting clinic.</p>
+            </div>
+        </div>
+    </div>
+    <div class="row  d-flex justify-content-center  align-items-center">
+
+        <div class="col-md-4 ">
+            <hr>
+        </div>
+        <div class="col-md-4 text-center">
+            <p class="line m-0">We ensure the partner clinic’s grow and build a brand for themselves.</p>
+        </div>
+        <div class="col-md-4">
+            <hr>
+        </div>
+    </div>
+
 </section>
+<!-- banner -->
+
+<!-- Explore -->
+<section class="pt-5">
+    <div class="bg" style="background-image: url('assets/img/about-banner.png'); max-width: 100%; background-repeat: no-repeat;height:110vh;">
+        <div class="row d-flex justify-content-center align-items-center">
+            <div class="col-md-5 text-center">
+                <p class="t1">Explore our <br>
+                    Integrated Marketplace.</p>
+            </div>
+        </div>
+
+        <div class="row pt-5 d-flex justify-content-center " style="position: relative;">
+            <div class="col-md-3 d-flex justify-content-center">
+                <img src="assets/img/phone1.png" alt="Phone 1" class="phone1">
+                <img src="assets/img/phone2.png" alt="Phone 2" class="phone2">
+                <div class="eicon1 ">
+                    <div class="row d-flex">
+                        <div class="col-md-2">
+                            <img src="assets/img/eicon1.png" alt="">
+                        </div>
+                        <div class="col-md-5">
+                            <p>Shop Your Prescription</p>
+                        </div>
+
+                    </div>
+                </div>
+                 <div class="eicon2 ">
+                    <div class="row d-flex">
+                        
+                        <div class="col-md-3">
+                            <img src="assets/img/eicon2.png" alt="">
+                        </div>
+                        <div class="col-md-5 ">
+                            <p>Explore Offers</p>
+                        </div>
+
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Explore -->
